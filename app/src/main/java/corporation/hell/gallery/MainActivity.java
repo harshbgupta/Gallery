ad43f519f18d69d1c9bf84af04c94c3cc7db9163
Harsh Gupta <lucifer007h@gmail.com>
2017-02-01 23:25:48 +0530

    Completed Gallery Demo Project
    Just Completed as Demo Level .  :-) :+1:

---
+++ app/src/main/java/corporation/hell/gallery/MainActivity.java
@@ -0,0 +1,211 @@
+package corporation.hell.gallery;
+
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.Gallery;
+import android.widget.ImageView;
+
+import android.app.Activity;
+import android.database.Cursor;
+import android.net.Uri;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.provider.MediaStore.MediaColumns;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.BaseAdapter;
+import android.widget.GridView;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.bumptech.glide.Glide;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    /** The images. */
+    private ArrayList<String> images;
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        GridView gallery = (GridView) findViewById(R.id.galleryGridView);
+
+        gallery.setAdapter(new ImageAdapter(this));
+
+        gallery.setOnItemClickListener(new OnItemClickListener() {
+
+            @Override
+            public void onItemClick(AdapterView<?> arg0, View arg1,
+                                    int position, long arg3) {
+                if (null != images && !images.isEmpty())
+                    Toast.makeText(getApplicationContext(),"position " + position + " " + images.get(position),Toast.LENGTH_SHORT).show();
+            }
+        });
+
+    }
+
+    /**
+     * The Class ImageAdapter.
+     */
+    private class ImageAdapter extends BaseAdapter {
+
+        /** The context. */
+        private Activity context;
+
+        /**
+         * Instantiates a new image adapter.
+         *
+         * @param localContext
+         *            the local context
+         */
+        public ImageAdapter(Activity localContext) {
+            context = localContext;
+            images = getAllShownImagesPath(context);
+        }
+
+        public int getCount() {
+            return images.size();
+        }
+
+        public Object getItem(int position) {
+            return position;
+        }
+
+        public long getItemId(int position) {
+            return position;
+        }
+
+        public View getView(final int position, View convertView,
+                            ViewGroup parent) {
+            ImageView picturesView;
+            if (convertView == null) {
+                picturesView = new ImageView(context);
+                picturesView.setScaleType(ImageView.ScaleType.FIT_CENTER);
+                picturesView
+                        .setLayoutParams(new GridView.LayoutParams(270, 270));
+
+            } else {
+                picturesView = (ImageView) convertView;
+            }
+
+            Glide.with(context).load(images.get(position))
+                    .placeholder(R.drawable.cert1).centerCrop()
+                    .into(picturesView);
+
+            return picturesView;
+        }
+
+        /**
+         * Getting All Images Path.
+         *
+         * @param activity
+         *            the activity
+         * @return ArrayList with images Path
+         */
+        private ArrayList<String> getAllShownImagesPath(Activity activity) {
+            Uri uri;
+            Cursor cursor;
+            int column_index_data, column_index_folder_name;
+            ArrayList<String> listOfAllImages = new ArrayList<String>();
+            String absolutePathOfImage = null;
+            uri = android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI;
+
+            String[] projection = { MediaColumns.DATA,
+                    MediaStore.Images.Media.BUCKET_DISPLAY_NAME };
+
+            cursor = activity.getContentResolver().query(uri, projection, null,
+                    null, null);
+
+            column_index_data = cursor.getColumnIndexOrThrow(MediaColumns.DATA);
+            column_index_folder_name = cursor
+                    .getColumnIndexOrThrow(MediaStore.Images.Media.BUCKET_DISPLAY_NAME);
+            while (cursor.moveToNext()) {
+                absolutePathOfImage = cursor.getString(column_index_data);
+
+                listOfAllImages.add(absolutePathOfImage);
+            }
+            return listOfAllImages;
+        }
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+   /* Integer[] pics = {
+            R.drawable.cert1,
+            R.drawable.cert2,
+            R.drawable.cert3,
+            R.drawable.cert4,
+            R.drawable.cert5
+    };
+    ImageView imageView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Gallery ga = (Gallery)findViewById(R.id.gallery);
+        ga.setAdapter(new ImageAdapter(this));
+    }
+
+    public class ImageAdapter extends BaseAdapter {
+
+        private Context ctx;
+        int imageBackground;
+
+        public ImageAdapter(Context c) {
+            ctx = c;
+            //TypedArray ta = obtainStyledAttributes(R.styleable.Gallery1);
+            //imageBackground = ta.getResourceId(R.styleable.Gallery1_android_galleryItemBackground, 1);
+            //ta.recycle();
+        }
+
+        @Override
+        public int getCount() {
+
+            return pics.length;
+        }
+
+        @Override
+        public Object getItem(int arg0) {
+
+            return arg0;
+        }
+
+        @Override
+        public long getItemId(int arg0) {
+
+            return arg0;
+        }
+
+        @Override
+        public View getView(int arg0, View arg1, ViewGroup arg2) {
+            ImageView iv = new ImageView(ctx);
+            iv.setImageResource(pics[arg0]);
+            iv.setScaleType(ImageView.ScaleType.FIT_XY);
+            //iv.setLayoutParams(new Gallery.LayoutParams(750,1020));
+            iv.setBackgroundResource(imageBackground);
+            return iv;
+        }
+
+    }*/
+}
