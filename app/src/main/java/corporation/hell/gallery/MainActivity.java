0dc5c0a7098789303e7080d029020a6387695d4c
Harsh Gupta <lucifer007h@gmail.com>
2017-02-02 00:11:40 +0530

    Minor changes

--- app/src/main/java/corporation/hell/gallery/MainActivity.java
+++ app/src/main/java/corporation/hell/gallery/MainActivity.java
@@ -1,21 +1,12 @@
 package corporation.hell.gallery;
 
-import android.content.Context;
-import android.content.res.TypedArray;
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.BaseAdapter;
-import android.widget.Gallery;
-import android.widget.ImageView;
-
 import android.app.Activity;
 import android.database.Cursor;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.provider.MediaStore.MediaColumns;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
@@ -42,13 +33,14 @@ public class MainActivity extends AppCompatActivity {
 
         gallery.setAdapter(new ImageAdapter(this));
 
+        //Setting Click
         gallery.setOnItemClickListener(new OnItemClickListener() {
 
             @Override
             public void onItemClick(AdapterView<?> arg0, View arg1,
                                     int position, long arg3) {
                 if (null != images && !images.isEmpty())
-                    Toast.makeText(getApplicationContext(),"position " + position + " " + images.get(position),Toast.LENGTH_SHORT).show();
+                    Toast.makeText(getApplicationContext(),"Images selectd of " + position,Toast.LENGTH_SHORT).show();
             }
         });
 
@@ -98,6 +90,7 @@ public class MainActivity extends AppCompatActivity {
                 picturesView = (ImageView) convertView;
             }
 
+            //Setting images to grid view
             Glide.with(context).load(images.get(position))
                     .placeholder(R.drawable.cert1).centerCrop()
                     .into(picturesView);
@@ -118,7 +111,7 @@ public class MainActivity extends AppCompatActivity {
             int column_index_data, column_index_folder_name;
             ArrayList<String> listOfAllImages = new ArrayList<String>();
             String absolutePathOfImage = null;
-            uri = android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI;
+            uri = MediaStore.Images.Media.EXTERNAL_CONTENT_URI;
 
             String[] projection = { MediaColumns.DATA,
                     MediaStore.Images.Media.BUCKET_DISPLAY_NAME };
@@ -138,74 +131,4 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-
-
-
-
-
-
-
-
-
-
-
-
-   /* Integer[] pics = {
-            R.drawable.cert1,
-            R.drawable.cert2,
-            R.drawable.cert3,
-            R.drawable.cert4,
-            R.drawable.cert5
-    };
-    ImageView imageView;
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        Gallery ga = (Gallery)findViewById(R.id.gallery);
-        ga.setAdapter(new ImageAdapter(this));
-    }
-
-    public class ImageAdapter extends BaseAdapter {
-
-        private Context ctx;
-        int imageBackground;
-
-        public ImageAdapter(Context c) {
-            ctx = c;
-            //TypedArray ta = obtainStyledAttributes(R.styleable.Gallery1);
-            //imageBackground = ta.getResourceId(R.styleable.Gallery1_android_galleryItemBackground, 1);
-            //ta.recycle();
-        }
-
-        @Override
-        public int getCount() {
-
-            return pics.length;
-        }
-
-        @Override
-        public Object getItem(int arg0) {
-
-            return arg0;
-        }
-
-        @Override
-        public long getItemId(int arg0) {
-
-            return arg0;
-        }
-
-        @Override
-        public View getView(int arg0, View arg1, ViewGroup arg2) {
-            ImageView iv = new ImageView(ctx);
-            iv.setImageResource(pics[arg0]);
-            iv.setScaleType(ImageView.ScaleType.FIT_XY);
-            //iv.setLayoutParams(new Gallery.LayoutParams(750,1020));
-            iv.setBackgroundResource(imageBackground);
-            return iv;
-        }
-
-    }*/
 }
